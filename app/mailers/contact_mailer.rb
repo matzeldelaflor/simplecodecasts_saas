275be0a5ecbdc1fb2a6910a41197dd22d3ebcbf6
Mark Matzeldelaflor <matzeldelaflor@gmail.com>
2015-07-19 07:07:28 +0000

    fixed code typo line 1 contact_mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,9 +1,9 @@
-class ContactMailer < ActionMailer
-    default to: 'matzeldelaflor@gmail.com'
-    def contact_email(name, email, body)
-        @name = name
-        @email = email
-        @body = body
-        mail(from: email, subject: 'Contact Form Message')
-    end
+class ContactMailer < ActionMailer::Base
+  default to: 'matzeldelaflor@gmail.com'
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
+    mail(from: email, subject: 'Contact Form Message')
+  end
 end
