9d781b9c717e0fdae72bd9dfba0f8f90319d9269
Mark Matzeldelaflor <matzeldelaflor@gmail.com>
2015-07-19 06:04:14 +0000

    Added mailer and view

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,9 @@
+class ContactMailer < ActionMailer
+    default to: 'matzeldelaflor@gmail.com'
+    def contact_email(name, email, body)
+        @name = name
+        @email = email
+        @body = body
+        mail(from: email, subject: 'Contact Form Message')
+    end
+end
